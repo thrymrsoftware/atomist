488fc7b328d1500fea0dedc06973ca07cf218f07
Rishi Khare <rishi@thrymr.net>
2017-12-23 13:37:30 +0000

    Initial commit from Atomist

---
+++ src/main/java/net/thrymr/AtomistApplication.java
@@ -0,0 +1,12 @@
+package net.thrymr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomistApplication.class, args);
+	}
+}
